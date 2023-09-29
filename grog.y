fe98cc9ae55b81c8fc6a876d9f8293fdb37c1f6e
Martin Straus <martinstraus@gmail.com>
2023-09-29 00:25:16 -0300

    Refactor to generate AST.

--- grog.y
+++ grog.y
@@ -2,12 +2,13 @@
 #include <stdio.h>
 #include "grog_ast.h"
 
-int result;
+Node* root;
 
 %}
 
 %union {
     int number;
+    struct Node* node;
 }
 
 /* Define tokens */
@@ -15,26 +16,26 @@ int result;
 %token ADD SUB MUL DIV
 %token EOL
 
-%type <number> calc expr term factor
+%type <node> calc expr term factor
 
 %%
 
 /* Start symbol */
-calc: expr EOL { result = $1; }
+calc: expr EOL { root = $1; }
     ;
 
 /* Expressions */
-expr: expr ADD term { $$ = $1 + $3; }
-    | expr SUB term { $$ = $1 - $3; }
+expr: expr ADD term { $$ = createExprNode(NT_ADD, $1, $3); }
+    | expr SUB term { $$ = createExprNode(NT_SUB, $1, $3); }
     | term { $$ = $1; }
     ;
 
-term: term MUL factor { $$ = $1 * $3; }
-    | term DIV factor { $$ = $1 / $3; }
+term: term MUL factor { $$ = createExprNode(NT_MUL, $1, $3); }
+    | term DIV factor { $$ = createExprNode(NT_DIV, $1, $3); }
     | factor { $$ = $1; }
     ;
 
-factor: NUMBER { $$ = $1; }
+factor: NUMBER { $$ = createIntNode($1); }
     ;
 
 %%
--- grogc.c
+++ grogc.c
@@ -4,10 +4,28 @@
 #include "grog_ast.h"
 
 extern FILE* yyin; // This is used to set the input source for the parser
-extern int result;
+extern Node* root;
 
 void showUsage() {
     fprintf(stderr, "Usage:\n\tgrogc [source file]\n");
+}
+
+int evaluate(Node *n) {
+    switch (n->type) {
+        case NT_NUMBER:
+            return n->number;
+        case NT_ADD:
+            return evaluate(n->left) + evaluate(n->right);
+        case NT_SUB:
+            return evaluate(n->left) - evaluate(n->right);
+        case NT_MUL:
+            return evaluate(n->left) * evaluate(n->right);
+        case NT_DIV:
+            return evaluate(n->left) - evaluate(n->right);
+        default:
+            fprintf(stderr, "Unsupported node type: %d\n", n->type);
+            exit(1);
+    }
 }
 
 int main(int args, char** argv) {
@@ -17,6 +35,7 @@ int main(int args, char** argv) {
     }
     yyin = fopen(argv[1], "r");
     yyparse();
+    int result = evaluate(root);
     printf("Result: %d\n", result);
     fclose(yyin);
     return 0;
